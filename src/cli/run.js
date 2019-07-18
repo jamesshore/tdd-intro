68af260744659c9431187f6791eaf3bf9ded81ee
James Shore <jshore@jamesshore.com>
2019-07-18 14:38:18 -0700

    Hook up simple CLI infrastructure

--- src/cli/run.js
+++ src/cli/run.js
@@ -1,4 +1,7 @@
 // Copyright Titanium I.T. LLC.
 "use strict";
 
-console.log("TODO", process.argv);
+const score = require("../score");
+
+const arg = process.argv[2];
+process.stdout.write(score.analyze(arg) + "\n");
