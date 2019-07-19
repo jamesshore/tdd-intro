0f1c7a1bbd08d5d2bcbf9b04fd1933c47821da54
James Shore <jshore@jamesshore.com>
2019-07-18 17:57:57 -0700

    Fix failed automatic describe experiment

--- src/_score_test.js
+++ src/_score_test.js
@@ -4,7 +4,7 @@
 const score = require("./score.js");
 const assert = require("chai").assert;
 
-describe(module.filename, function() {
+describe("Score", function() {
 
 	it("isn't implemented", function() {
 		assert.equal(score.analyze(), 19);
