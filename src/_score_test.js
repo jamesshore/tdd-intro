0c2f2eb002f29d63301f644d6ad0d583e20925a5
James Shore <jshore@jamesshore.com>
2019-07-18 22:16:22 -0700

    score.combo() scores straights

--- src/_score_test.js
+++ src/_score_test.js
@@ -8,12 +8,17 @@ const parse = require("./parse");
 describe("Score", function() {
 
 	it("scores pairs", function() {
-		assert.equal(score.combo(createCards("2H", "2D")), 2, "should score matching pair");
+		assert.equal(score.combo(createCards("2H", "2D")), 2, "should score pair");
 		assert.equal(score.combo(createCards("2H", "3D")), 0, "shouldn't score non-pair");
 		assert.equal(score.combo(createCards("2H", "2D", "4H")), 0, "shouldn't score three cards w/ pair");
-		assert.equal(score.combo(createCards("2H", "2D", "2S")), 0, "shouldn't score three triple");
+		assert.equal(score.combo(createCards("2H", "2D", "2S")), 0, "shouldn't score triple");
 	});
 
+	it("scores straights", function() {
+		assert.equal(score.combo(createCards("2H", "3D", "4S")), 3, "should score straight");
+		assert.equal(score.combo(createCards("2H", "3D", "5S")), 0, "shouldn't score non-straight");
+		assert.equal(score.combo(createCards("2H", "3D", "4S", "AC")), 4, "should score one point per card");
+	});
 });
 
 function createCards(...cardStrings) {
--- src/score.js
+++ src/score.js
@@ -7,8 +7,15 @@ exports.analyze = function(cardsString) {
 	return 19;
 };
 
-exports.combo = function(handCards) {
-	if (handCards.length !== 2) return 0;
-
-	return Card.isPair(handCards[0], handCards[1]) ? 2 : 0;
+exports.combo = function(cards) {
+	return scorePair(cards) + scoreStraight(cards);
 };
+
+function scorePair(cards) {
+	if (cards.length !== 2) return 0;
+	return Card.isPair(cards[0], cards[1]) ? 2 : 0;
+}
+
+function scoreStraight(cards) {
+	return Card.isStraight(cards) ? cards.length : 0;
+}
