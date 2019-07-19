8ecb9f2bdbb7684757cd452d87635f5448138e38
James Shore <jshore@jamesshore.com>
2019-07-18 22:04:53 -0700

    Add Card.isStraight()

--- src/_card_test.js
+++ src/_card_test.js
@@ -18,6 +18,38 @@ describe("Card", function() {
 		assert.isFalse(Card.isPair(nineSpades, eightHearts), "should recognize non-pairs");
 	});
 
+	it("recognizes straights", function() {
+		const aceSpades = createHandCard("AS");
+		const twoSpades = createHandCard("2S");
+		const twoSpadesStarter = createStarterCard("2S");
+		const threeSpades = createHandCard("3S");
+		const fourSpades = createHandCard("4S");
+		const fiveSpades = createHandCard("5S");
+		const jackSpades = createHandCard("JS");
+		const queenSpades = createHandCard("QS");
+		const kingSpades = createHandCard("KS");
+
+		assert.isTrue(Card.isStraight([ twoSpades, threeSpades, fourSpades ]), "should recognize straights");
+		assert.isTrue(
+			Card.isStraight([ twoSpadesStarter, threeSpades, fourSpades ]),
+			"shouldn't care about starter status"
+		);
+		assert.isFalse(Card.isStraight([ twoSpades, threeSpades ]), "should require at least three cards");
+		assert.isFalse(Card.isStraight([ twoSpades, threeSpades, fiveSpades ]), "should recognize non-straights");
+		assert.isTrue(Card.isStraight([ jackSpades, queenSpades, kingSpades ]), "should recognize face cards");
+		assert.isTrue(Card.isStraight([ aceSpades, twoSpades, threeSpades ]), "should recognize ace");
+		assert.isFalse(Card.isStraight([ jackSpades, queenSpades, aceSpades ]), "shouldn't think ace is high");
+		assert.isTrue(
+			Card.isStraight([ aceSpades, twoSpades, threeSpades, fourSpades, fiveSpades ]),
+			"should handle more than three cards"
+		);
+		assert.isFalse(
+			Card.isStraight([ aceSpades, twoSpades, threeSpades, fourSpades, kingSpades ]),
+			"should require all cards to be part of straight"
+		);
+		assert.isTrue(Card.isStraight([ threeSpades, twoSpades, fourSpades ]), "should handle out-of-order cards");
+	});
+
 });
 
 function createHandCard(cardStr) {
--- src/card.js
+++ src/card.js
@@ -1,6 +1,22 @@
 // Copyright Titanium I.T. LLC.
 "use strict";
 
+const NUMERIC_RANKS = {
+	"A": 1,
+	"2": 2,
+	"3": 3,
+	"4": 4,
+	"5": 5,
+	"6": 6,
+	"7": 7,
+	"8": 8,
+	"9": 9,
+	"10": 10,
+	"J": 11,
+	"Q": 12,
+	"K": 13,
+};
+
 module.exports = class Card {
 	static get RANKS() { return ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]; }
 	static get SUITS() { return ["C", "S", "H", "D"]; }
@@ -14,4 +30,23 @@ module.exports = class Card {
 	static isPair(card1, card2) {
 		return card1._rank === card2._rank;
 	}
+
+	static isStraight(cards) {
+		if (cards.length < 3) return false;
+
+		cards.sort((a, b) => numericRank(a) - numericRank(b));
+
+		let prevRank = numericRank(cards[0]);
+		let result = true;
+		for (let i = 1; i < cards.length; i++) {
+			const currentRank = numericRank(cards[i]);
+			if (currentRank - 1 !== prevRank) result = false;
+			prevRank = currentRank;
+		}
+		return result;
+	}
 };
+
+function numericRank(card) {
+	return NUMERIC_RANKS[card._rank];
+}
