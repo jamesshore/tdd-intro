5aea470937b911daa5023452e1132d55787ebacc
James Shore <jshore@jamesshore.com>
2019-07-18 21:01:35 -0700

    score.combo() scores pairs

--- src/_score_test.js
+++ src/_score_test.js
@@ -1,13 +1,21 @@
 // Copyright Titanium I.T. LLC.
 "use strict";
 
-const score = require("./score.js");
+const score = require("./score");
 const assert = require("chai").assert;
+const parse = require("./parse");
 
 describe("Score", function() {
 
-	it("isn't implemented", function() {
-		assert.equal(score.analyze(), 19);
+	it("scores pairs", function() {
+		assert.equal(score.combo(createCards("2H", "2D")), 2, "should score matching pair");
+		assert.equal(score.combo(createCards("2H", "3D")), 0, "shouldn't score non-pair");
+		assert.equal(score.combo(createCards("2H", "2D", "4H")), 0, "shouldn't score three cards w/ pair");
+		assert.equal(score.combo(createCards("2H", "2D", "2S")), 0, "shouldn't score three triple");
 	});
 
 });
+
+function createCards(...cardStrings) {
+	return cardStrings.map((cardString) => parse.card(cardString));
+}
--- src/score.js
+++ src/score.js
@@ -1,6 +1,14 @@
 // Copyright Titanium I.T. LLC.
 "use strict";
 
+const Card = require("./card");
+
 exports.analyze = function(cardsString) {
 	return 19;
+};
+
+exports.combo = function(handCards) {
+	if (handCards.length !== 2) return 0;
+
+	return Card.isPair(handCards[0], handCards[1]) ? 2 : 0;
 };
